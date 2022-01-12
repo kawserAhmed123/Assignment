3f7a1d61da93b35d7c0f7332fa49bb384fd15dac
ashraf-pitu <57963000+ashraf-pitu@users.noreply.github.com>
2022-01-12 23:40:20 +0600

    changes made here

--- Assignment1/RectangleAssignment.java
+++ Assignment1/RectangleAssignment.java
@@ -3,7 +3,7 @@ class FindRectangleArea
 {
     public static void main(String arg[])
     {
-        Rectangle rect = new Rectangle(10, 5);
+        Rectangle rect = new Rectangle(10, 6);
         
         System.out.println("Length = " + rect.length);
         System.out.println("Breadth = " + rect.breadth);
