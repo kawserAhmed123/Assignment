b2b7133e0b52ff3fffc11f0eadff1d765fdf18fb
ashraf-pitu <57963000+ashraf-pitu@users.noreply.github.com>
2022-01-12 23:04:21 +0600

    Update RectangleAssignment.java

--- Assignment1/RectangleAssignment.java
+++ Assignment1/RectangleAssignment.java
@@ -1,11 +1,14 @@
 
-public class RectangleAssignment {
-    public static void main(String[] args) {
-        Rectangle ob =new Rectangle(7,8);
-        double result1= ob.calculateArea(7,8);
-        double result2= ob.calculatePerimeter(7,8);
-        System.out.print("The area of the rectangle is:"+ result1);
-        System.out.println("  The area of the primetr is:"+ result2);
-    }
+class FindRectangleArea
+{
+    public static void main(String arg[])
+    {
+        Rectangle rect = new Rectangle(10, 5);
+        
+        System.out.println("Length = " + rect.length);
+        System.out.println("Breadth = " + rect.breadth);
+        System.out.println("Area = " + rect.getArea());
+        System.out.println("Perimeter = " + rect.getPerimeter());
     
+    }
 }
